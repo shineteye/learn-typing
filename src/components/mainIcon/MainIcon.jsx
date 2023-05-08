dae7816cb710cb79b88ad0cd47f5fe9983538098
Teye Shine <60197099+shineteye@users.noreply.github.com>
2023-05-08 02:15:05 +0000

    icon

--- src/components/mainIcon/MainIcon.jsx
+++ src/components/mainIcon/MainIcon.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import TypingLogo from '../../assets/imgs/blog_480px.png'
 export default function MainIcon() {
     return (
-        <div className='flex flex-row justify-between items-center mx-3 md:w-1/2'>
+        <div className='flex flex-row items-center mx-3 md:w-1/2'>
             <img className='w-12 mx-3' src={TypingLogo} alt="TypingLogo" />
             <h3 className='text-2xl font-bold text-darkBlue'>Learn-Typing</h3>
         </div>
