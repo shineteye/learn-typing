dd957a7652aba7a0dea8245587c40b21deaeb9b3
Teye Shine <60197099+shineteye@users.noreply.github.com>
2023-05-08 03:33:50 +0000

    landing page alignment

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -7,18 +7,17 @@ export default function LandingPage() {
         <div className='container relative flex-col mx-auto'>
             <Navbar />
             {/* Header */}
-            <div className='flex flex-col items-center my-24  md:flex-row-reverse'>
-                <img className='' src={Keyboard} alt="keyboardimage" />
+            <div className='flex flex-col items-center my-24 justify-between  md:flex-row-reverse'>
+                <img className='md:w-1/2' src={Keyboard} alt="keyboardimage" />
                 {/* Left Content */}
                 <div className='flex flex-col items-center max-w-md md:w-1/2'>
-                    <h1 className='text-4xl font-bold text-darkBlue my-10 text-center md:text-left'>
+                    <h1 className='text-4xl font-bold text-darkBlue my-10 text-center md:text-left md:self-start'>
                         What's Our Mission
                     </h1>
-                    <p className='text-xl md:text-3xl md:text-left'>
+                    <p className='text-xl self-center md:text-3xl md:text-left '>
                         We exist only to boost your typing speed
                     </p>
                 </div>
-
             </div>
         </div>
     )
